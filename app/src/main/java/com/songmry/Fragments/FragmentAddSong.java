ac41e7189c00cbe07b88d2895207c087c98452bd
Samuel Garcia <evileumas@gmail.com>
2016-01-06 02:23:55 -0800

    Se elimino un metodo comentado innecesario

--- app/src/main/java/com/songmry/Fragments/FragmentAddSong.java
+++ app/src/main/java/com/songmry/Fragments/FragmentAddSong.java
@@ -191,33 +191,3 @@ public class FragmentAddSong extends Fragment {
 
     }
 }
-
-/*private static void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
-
-        System.out.println("\n=============================================================");
-        System.out.println(
-                "   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
-        System.out.println("=============================================================\n");
-
-        if (!iteratorSearchResults.hasNext()) {
-            System.out.println(" There aren't any results for your query.");
-        }
-
-        while (iteratorSearchResults.hasNext()) {
-
-            SearchResult singleVideo = iteratorSearchResults.next();
-            ResourceId rId = singleVideo.getId();
-
-            // Confirm that the result represents a video. Otherwise, the
-            // item will not contain a video ID.
-            if (rId.getKind().equals("youtube#video")) {
-                Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().getDefault();
-
-                System.out.println(" Video Id" + rId.getVideoId());
-                System.out.println(" Title: " + singleVideo.getSnippet().getTitle());
-                System.out.println(" Thumbnail: " + thumbnail.getUrl());
-                System.out.println("\n-------------------------------------------------------------\n");
-
-            }
-        }
-    }*/
